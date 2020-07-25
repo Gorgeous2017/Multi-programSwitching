445a7c72de51b3f56ba6fee32a0354ddb4bf443e
Gump Jenson <ganjunsen@qq.com>
2020-07-25 10:24:46 +0800

    feat: bootloader project demo

--- targets/STM32L431_BearPi/Src/main.c
+++ targets/STM32L431_BearPi/Src/main.c
@@ -60,24 +60,52 @@ VOID HardWare_Init(VOID)
 	LCD_ShowString(10, 170, 240, 16, 16, "Please wait for system init");
 }
 
+void JumpAPP_test(void) {
+    
+    typedef  void (*iapfun)(void); /* 函数指针实现APP工程寻址与程序跳转 */
+
+	uint32_t JUMP_ADDR = 0x08004000; /* APP程序基地址 */
+	uint32_t STACK_ADDR = 0x20000000; /* APP程序栈地址 */
+	uint32_t RESET_IRQ_ADDR = JUMP_ADDR + 4; /* 中断向量表地址 */
+
+	iapfun jump2app; /* 声明跳转函数 */
+
+	jump2app = (iapfun)*(volatile uint32_t *)RESET_IRQ_ADDR; /* APP地址赋值 */
+	__set_MSP(STACK_ADDR); /* 设置APP的栈顶 */
+	__disable_irq(); /* 失能中断 */
+
+    printf("APP Jump above \n");
+
+	jump2app(); /* APP程序跳转 */
+
+}
+
 int main(void)
 {
-    UINT32 uwRet = LOS_OK;
+    // UINT32 uwRet = LOS_OK;
     HardWare_Init();
-    uwRet = LOS_KernelInit();
-    if (uwRet != LOS_OK)
-    {
-        return LOS_NOK;
-    }
 
-    extern UINT32 create_work_tasks(VOID);
-    uwRet = create_work_tasks();
-    if (uwRet != LOS_OK)
-    {
-        return LOS_NOK;
-    }
+    printf("BootLoader Project test \n");
 
+    HAL_Delay(2000);
 
-    (void)LOS_Start();
+    JumpAPP_test();
+
+    printf("APP Jump below \n");
+
+    // uwRet = LOS_KernelInit();
+    // if (uwRet != LOS_OK)
+    // {
+    //     return LOS_NOK;
+    // }
+
+    // extern UINT32 create_work_tasks(VOID);
+    // uwRet = create_work_tasks();
+    // if (uwRet != LOS_OK)
+    // {
+    //     return LOS_NOK;
+    // }
+
+    // (void)LOS_Start();
     return 0;
 }
