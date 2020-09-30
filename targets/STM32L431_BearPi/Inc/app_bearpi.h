6ce3d32bebfa475157e565f6d4908438fb9b855e
Gump Jenson <ganjunsen@qq.com>
2020-09-30 23:53:52 +0800

    feat: combine the bearpi example file

--- targets/STM32L431_BearPi/Inc/app_bearpi.h
+++ targets/STM32L431_BearPi/Inc/app_bearpi.h
@@ -16,12 +16,24 @@
 #define __APP_BEARPI_H__ 
 
 /* Includes ------------------------------------------------------------------*/
+#include "los_base.h"
+#include "los_task.ph"
+#include "los_typedef.h"
+#include "los_sys.h"
+
+#include "E53_SC1.h"
+#include "E53_IA1.h"
+#include "E53_ST1.h"
+#include "E53_SF1.h"
 
 /* Exported typedef -----------------------------------------------------------*/
 
 /* Exported constants --------------------------------------------------------*/
 
 /* Exported function -----------------------------------------------*/
+UINT32 creat_sc1_collection_task();
+UINT32 creat_ia1_collection_task();
+
 
 #endif /* __APP_BEARPI_H__ */ 
 /********************************** END OF FILE *******************************/
